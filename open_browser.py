97c6fdddbd8c1843dded6f09f5ed7fc1868677c7
Maxb <maxb@yinhe.com>
2018-05-02 11:12:48 +0800

    modify open_browser.py

--- open_browser.py
+++ open_browser.py
@@ -3,10 +3,10 @@ import time
 
 request_url = input("Please enter the url:")	#获取url
 
-driver = webdriver.Chrome()		#初始化浏览器
+driver = webdriver.Firefox()		#初始化浏览器
 
 driver.maximize_window()		#浏览器最大化
 
 time.sleep(5) 		#延迟
 
-driver.get(request_url)		#跳转输入url
+driver.get(request_url)		#跳转输入urlxi
