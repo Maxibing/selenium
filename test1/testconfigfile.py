8a46d0ca49a481c162600da08b0f4b83e8350c6b
Maxb <maxb@yinhe.com>
2018-05-09 19:25:29 +0800

    add testconfigfile.py

---
+++ test1/testconfigfile.py
@@ -0,0 +1,20 @@
+#coding=utf-8
+import configparser
+import os
+
+class TestReadConfigFile(object):
+
+    def get_value(self):
+        root_dir = os.path.dirname(os.path.abspath('.'))    #获取项目根目录的相对路径
+        print(root_dir)
+        config = configparser.ConfigParser()
+        file_path = os.path.dirname(os.path.abspath('.')) + '/config/config.ini'
+        config.read(file_path)
+
+        browser = config.get('Browsertype', 'browserName')
+        url = config.get('ServerURL', 'URL')
+
+        return (browser, url)  # 返回的是一个元组
+
+trcf = TestReadConfigFile()
+print(trcf.get_value())
